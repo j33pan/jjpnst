bdbb23145c1165e8f19eb1d7ef64a567f7594a88
Juejie Pan <panapril77@gmail.com>
2021-05-07 23:07:39 -0400

    stripe lambda auth permission

--- amplify/backend/function/jjpstripepay/src/index.js
+++ amplify/backend/function/jjpstripepay/src/index.js
@@ -1,3 +1,14 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_JJPNST1BF6237D_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+const { CognitoIdentityServiceProvider } = require("aws-sdk");
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+const USER_POOL_ID = process.env.AUTH_JJPNST1BF6237D_USERPOOLID;
+const stripe = require("stripe")("<strip_private_key>");
+
 exports.handler = async (event) => {
   // TODO implement
   const response = {
