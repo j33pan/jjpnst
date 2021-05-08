6e72883bc51d0bb9541ac5f82efb68b55e268bd8
Juejie Pan <panapril77@gmail.com>
2021-05-07 22:52:28 -0400

    add layer and db permission to lamda function & put to table

--- amplify/backend/function/jjpcreateorder/src/index.js
+++ amplify/backend/function/jjpcreateorder/src/index.js
@@ -1,15 +1,50 @@
+/* Amplify Params - DO NOT EDIT
+	API_JJPNST_GRAPHQLAPIIDOUTPUT
+	API_JJPNST_JJPORDERPRODUCTTABLE_ARN
+	API_JJPNST_JJPORDERPRODUCTTABLE_NAME
+	API_JJPNST_JJPORDERTABLE_ARN
+	API_JJPNST_JJPORDERTABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
 
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+
+const params = {
+  TableName: process.env.API_JJPNST_JJPORDERPRODUCTTABLE_NAME,
+  /* Item properties will depend on your application concerns */
+  Item: {
+    id: uuidv4(),
+    code: "some code...",
+  },
+};
+
+async function createItem() {
+  try {
+    await docClient.put(params).promise();
+  } catch (err) {
+    return err;
+  }
+}
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  try {
+    await createItem();
+    return { body: "Successfully created item!" };
+  } catch (err) {
+    return { error: err };
+  }
+  //   // TODO implement
+  //   const response = {
+  //     statusCode: 200,
+  //     //  Uncomment below to enable CORS requests
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Headers": "*",
+  //     },
+  //     body: JSON.stringify("Hello from Lambda!"),
+  //   };
+  //   return response;
 };
